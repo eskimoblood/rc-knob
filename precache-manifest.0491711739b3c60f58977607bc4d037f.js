43b784f7c8dc032cc0799fed063eb2b070893b02
eskimoblood <eskimoblood@users.noreply.github.com>
2019-12-31 12:45:54 +0000

    Automated deployment: Tue Dec 31 12:45:54 UTC 2019 65f29e182910631290a5e081a1ad1bd239655ec5

---
+++ precache-manifest.0491711739b3c60f58977607bc4d037f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "19f2241b9c59cc2ccd1a",
+    "url": "/rc-knob/static/js/runtime~main.19f2241b.js"
+  },
+  {
+    "revision": "110ec249442320f37631",
+    "url": "/rc-knob/static/js/main.110ec249.chunk.js"
+  },
+  {
+    "revision": "d0fa24bdc55587331d2e",
+    "url": "/rc-knob/static/js/1.d0fa24bd.chunk.js"
+  },
+  {
+    "revision": "110ec249442320f37631",
+    "url": "/rc-knob/static/css/main.215ee603.chunk.css"
+  },
+  {
+    "revision": "42be79e71443f079acc2e6b1b3eb8f1b",
+    "url": "/rc-knob/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/rc-knob/precache-manifest.0491711739b3c60f58977607bc4d037f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/rc-knob/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
